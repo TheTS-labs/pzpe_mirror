887efdb53e9cb4115eb49ae5b2dc328bcc6e81b8
Roman Tovt <romantovt31@gmail.com>
2026-04-11 07:54:02 +0300

    Add link support in schedule and fix sizing

--- app/components/index/Schedule.tsx
+++ app/components/index/Schedule.tsx
@@ -3,12 +3,14 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "..
 import { Separator } from "~/components/ui/separator";
 import React from "react";
 
+const URL_REGEX = /(https?:\/\/[^\s]+)/g;
+
 export interface ScheduleProps {
     schedule: Schedule | undefined
 }
 
 export default function Schedule(props: ScheduleProps) {
-    return <div className="flex flex-col gap-5">
+    return <div className="flex flex-col gap-5 w-full max-w-2xl mx-auto px-4">
         {props.schedule && Object.entries(props.schedule).map(([date, lessons]) => <React.Fragment key={date}>
             <div>
                 <h1 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">{date}</h1>
@@ -19,8 +21,19 @@ export default function Schedule(props: ScheduleProps) {
                             <b>{lesson.time.start} - {lesson.time.end}</b>: {lesson.subject.full} <span className="text-muted-foreground">[{lesson.subject.type}]</span>
                         </AccordionTrigger>
                         <AccordionContent className="pl-4">
-                            {lesson.notice.split("\n").map((line, i) => <p key={i}>{line}</p>)}
+                            {lesson.notice.trim().split("\n").map((line, i) => <p key={i}>
+                                {line.split(URL_REGEX).map((part, j) => part.match(URL_REGEX) ? <a 
+                                    key={j} 
+                                    href={part} 
+                                    target="_blank" 
+                                    rel="noopener noreferrer"
+                                >
+                                    {part}
+                                </a> : part)}
+                            </p>)}
+
                             <br />
+
                             <p className="text-neutral-500 italic">
                                     Teacher: <span className="font-bold">{lesson.teacher}</span>
                                 <br />
