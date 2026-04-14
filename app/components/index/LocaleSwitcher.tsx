4bd5b8437eb5e8368546f16e6cc7c048518c719d
Roman Tovt <romantovt31@gmail.com>
2026-04-14 23:28:49 +0300

    Preserve URL search params in LocaleSwitcher

--- app/components/index/LocaleSwitcher.tsx
+++ app/components/index/LocaleSwitcher.tsx
@@ -3,13 +3,15 @@ import { useLocale } from "react-intlayer";
 import { Link, useLocation } from "react-router";
 
 function LocaleLink(props: { locale: LocalesValues, name: string }) {
-    const { pathname } = useLocation();
+    const { pathname, search } = useLocation(); 
     const { locale: currentLocale, setLocale } = useLocale();
+    
     const pathWithoutLocale = getPathWithoutLocale(pathname);
+    const localizedPath = getLocalizedUrl(pathWithoutLocale, props.locale);
 
     return <Link
         onClick={() => setLocale(props.locale)}
-        to={getLocalizedUrl(pathWithoutLocale, props.locale)}
+        to={`${localizedPath}${search}`}
         className={currentLocale == props.locale ? "text-indigo-500 font-bold" : ""}
     >
         {props.name}
@@ -20,9 +22,7 @@ export default function LocaleSwitcher() {
     return (
         <div className="flex flex-row gap-2">
             <LocaleLink locale={Locales.ENGLISH_UNITED_STATES} name="English" />
-
             <p>|</p>
-
             <LocaleLink locale={Locales.UKRAINIAN_UKRAINE} name="Українська" />
         </div>
     );
